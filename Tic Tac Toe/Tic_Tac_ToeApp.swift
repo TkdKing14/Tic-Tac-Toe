d35736ed99805669dcdbcddf8f4aac2a4fa6e613
Carson Payne <carson.j.payne14@gmail.com>
2024-11-22 09:30:42 -0600

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Carson Payne on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
